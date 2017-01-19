f4e741ad43cb2f7d3e87c7669d76970bcd4b581a
juwaini <juwaini@gmail.com>
2017-01-20 00:12:54 +0800

    Able to get json from API endpoint

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var koa = require('koa');
 var app = koa();
-var route = require('koa-route');
+var rp = require('request-promise');
 
 app.use(function *(){
-    this.body = {"hello": "world"};
+    var options = {
+        uri: 'https://jsonplaceholder.typicode.com/posts/1',
+        json: true
+    };
+
+    rp(options)
+        .then(function(res){
+            var output = {};
+            output.id = res.id;
+            output.title = res.title;
+            console.log(output);
+            this.body = output;
+            //console.log(this.body);
+            //return res;
+        })
+        .catch(function (err){
+            console.log(err);
+            this.body = err;
+        });
 });
 
 app.listen(3000);
